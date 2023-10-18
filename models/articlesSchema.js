7b6b757945c8abd7808f3b3d576a4cb992bc7aff
JiQunHuang <110572798+JiQunH@users.noreply.github.com>
2023-10-18 18:05:50 +1300

    Added new db function

---
+++ models/articlesSchema.js
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose';
+
+//Model of schema used for SPEED database
+
+//Must restart page (either local or on vercel) to see changes to enumerated fields
+//and other changes in here as this is cached.
+
+const ArticlesSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: false,
+    },
+    authors: {
+        type: [String],
+        required: false,
+    },
+    journalOrConferenceName: {
+        type: String,
+        required: false,
+    },
+    yearOfPublication: {
+        type: Number,
+        required: false,
+    },
+    volume: {
+        type: Number,
+        required: false,
+    },
+    number: {
+        type: Number,
+        required: false,
+    },
+    pages: {
+        type: String,
+        required: false,
+    },
+    DOI: {
+        type: String,
+        required: false,
+    },
+    SEPractice: {
+        type: String,
+        required: false,
+    },
+    claim: {
+        type: String,
+        required: false,
+    },
+    resultOfEvidence: {
+        type: String,
+        enum: ['Agree', 'Disagree'], // enum used to limit to specified values
+        required: false,
+    },
+    typeOfResearch: {
+        type: String,
+        enum: ['Case Study', 'Experiment', 'Survey', 'Interview'], // enum used to limit to specified values
+        required: false,
+    },
+    typeOfParticipant: {
+        type: String,
+        enum: ['Student', 'Practitioner'], // enum used to limit to specified values
+        required: false,
+    },
+    analysisSummary: {
+        type: String,
+        required: false,
+    },
+});
+
+export default mongoose.models.Articles || mongoose.model('Articles', ArticlesSchema);
